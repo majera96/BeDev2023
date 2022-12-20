0191a49cba33ef185b2bb5cda3101fddabdaaed4
majera96 <majer.antonio@gmail.com>
2022-12-20 20:22:48 +0100

    Create vjezba2.php
    vjezba broj 2 sa sata

---
+++ osnovephp.hr/funckije/vjezba2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    //Proizvoljno deklarirajte funkciju koja ima dva argumenta(name i surname). Funkcija treba
+    //konkatenirati podatke iz argumenata tako da između postoji razmak i dodjeliti ih lokalnoj
+    //varijabli, zatim treba vrijednost u varijabli pretvoriti u velika slova i to vratiti kao rezultat.
+ 
+    function getNameAndSurname($name,$surname)
+ {
+    $nameAndSurname = $name . ' ' . $surname;
+    $nameAndSurname = strtoupper($nameAndSurname);
+    return $nameAndSurname;
+ }
+
+ //Pozovite funkciju i vraćebnu vrijednost dodjelite varijabli.
+
+ $text = getNameAndSurname('Antonio','Majer');
+
+ //Ispišite vrijednost varijable.
+
+ echo $text;
+
+    ?>
+</body>
+</html>
