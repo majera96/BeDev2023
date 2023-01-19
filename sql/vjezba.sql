5685266eba36ba5a7adb41e40a9edf13256800cb
majera96 <majer.antonio@gmail.com>
2023-01-19 20:18:51 +0100

    Create vjezba.sql
    sutra ostatak
    popravak errora sa password no za roota

---
+++ sql/vjezba.sql
@@ -0,0 +1,4 @@
+#C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\BeDev2023\sql\vjezba.sql
+drop database if exists vjezba;
+create database vjezba;
+use vjezba;
